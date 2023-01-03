7db40961de48bc7a3427c4dda9793256207e667c
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-01-03 18:59:56 -0500

    day 2 part b

--- src/Day2.hs
+++ src/Day2.hs
@@ -12,8 +12,9 @@ solve :: IO ()
 solve = do
   contents <- readFile "input/day2.txt"
   let input = map parseLine $ lines contents
-  print $ position input 0 0
-  
+  print $ solveA input
+  print $ solveB input
+
 parseLine :: String -> Command
 parseLine l = parse (T.splitOn " " (T.pack l))
 
@@ -25,11 +26,23 @@ parse [c,v] = command
         "down" -> Down value
         "forward" -> Forward value
     value = read $ T.unpack v
-        
-position :: [Command] -> Int -> Int -> Int
-position [] x y = x*y
-position c x y = case head c of
-  Up v -> position (tail c) x (y-v)
-  Down v -> position (tail c) x (y+v)
-  Forward v -> position (tail c) (x+v) y
 
+solveA :: [Command] -> Int
+solveA input = positionA input 0 0
+        
+positionA :: [Command] -> Int -> Int -> Int
+positionA [] x y = x*y
+positionA c x y = case head c of
+  Up v -> positionA (tail c) x (y-v)
+  Down v -> positionA (tail c) x (y+v)
+  Forward v -> positionA (tail c) (x+v) y
+
+solveB :: [Command] -> Int
+solveB input = positionB input 0 0 0
+        
+positionB :: [Command] -> Int -> Int -> Int -> Int
+positionB [] x y a = x*y
+positionB c x y a = case head c of
+  Up v -> positionB (tail c) x y (a-v)
+  Down v -> positionB (tail c) x y (a+v)
+  Forward v -> positionB (tail c) (x+v) (y+a*v) a
