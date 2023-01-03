a6dd8c9c43f58cf95dcd451ce229ce46a1ea6297
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-01-03 17:34:49 -0500

    day1 b

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,13 +1,29 @@
 module Day1 where
 
+import Data.List (tails)
+
 solve :: IO ()
 solve = do
   contents <- readFile "input/day1.txt"
   let input = map readInt . lines $ contents
   print . solveA $ input
+  print . solveB $ input
 
 solveA :: [Int] -> Int
-solveA input =  length . filter (>0) $ diff input
+solveA input = length . filter (>0) $ diff input
+
+solveB :: [Int] -> Int
+solveB input = length . filter (>0) $ diff sums
+    where sums = map sum $ windows 3 input
+
+windows' :: Int -> [a] -> [[a]]
+windows' n = map (take n) . tails
+
+takeLengthOf :: [a] -> [b] -> [b]
+takeLengthOf = zipWith (flip const)
+
+windows :: Int -> [a] -> [[a]]
+windows n xs = takeLengthOf (drop (n-1) xs) (windows' n xs)
 
 readInt :: String -> Int
 readInt = read
